b772b72747fa114b67328aded0d164a3121ac76e
Salja <salja2012@hotmail.de>
2013-02-05 15:32:42 +0100

    [z0080] Typo
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/202_updatepack_mangos.sql
+++ _develop/202_updatepack_mangos.sql
@@ -379,4 +379,4 @@ DELETE FROM creature_movement WHERE id NOT IN (SELECT guid FROM creature);
 DELETE FROM creature_addon WHERE guid NOT IN (SELECT guid FROM creature);
 
 --UPDATE Database Version
-UPDATE db_version set version = 'ZeroDatabase 2.0.2 for MaNGOSZero zXXXX+ and ScriptDev0 zXXXX+';
+UPDATE db_version set version = 'ZeroDatabase 2.0.2 for MaNGOSZero zXXXX+ and ScriptDevZero zXXXX+';
