aa1db3bc519cecd646710e6d704c189187e9ef88
Salja <salja2012@hotmail.de>
2013-02-05 16:56:51 +0100

    [z0082] Release updatepack 2.0.2
    NOTE:
    the next time we will provide small things faster
    
    CHANGELOG:
    -- fix missing graveyard for naxxramas
    -- fic naxxramas creature updates
    -- fix missing gameobject spawns (naxxramas)
    -- fix missing creature spawns (naxxramas)
    -- fix Quest 8519 (some misc updates)
    -- fix Quest 970 (fix droprate)
    -- fix Quest 963 (end script / make more blizzlike)
    
    Signed-off-by: Salja <salja2012@hotmail.de>

---
+++ _develop/203_updatepack_mangos.sql
@@ -0,0 +1,41 @@
+--
+-- Copyright (C) 2005-2013 MaNGOS <http://getmangos.com/>
+-- Copyright (C) 2009-2013 MaNGOSZero <https://github.com/mangoszero>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+
+-- drop conditions for recipes. each player can get any recipe (without a proper profession) Thanks kokeszko
+DELETE FROM `conditions` WHERE condition_entry IN (6,10,13,16);
+UPDATE creature_loot_template SET condition_id=0 WHERE condition_id IN (6,10,13,16);
+
+-- Cleanup
+UPDATE creature SET
+curhealth = (SELECT FLOOR(RAND(1)*(maxhealth - minhealth) + minhealth) FROM creature_template WHERE creature.id = creature_template.entry)
+WHERE curhealth < (SELECT minhealth FROM creature_template WHERE creature.id = creature_template.entry);
+
+UPDATE creature SET
+curmana = (SELECT FLOOR(RAND(1)*(maxmana - minmana) + minmana) FROM creature_template WHERE creature.id = creature_template.entry)
+WHERE curmana < (SELECT minmana FROM creature_template WHERE creature.id = creature_template.entry);
+
+UPDATE creature SET MovementType = 1 WHERE spawndist != 0 AND MovementType = 0;
+UPDATE creature SET MovementType = 0 WHERE spawndist = 0 AND MovementType != 2;
+UPDATE creature SET MovementType = 2 WHERE guid IN (SELECT DISTINCT id FROM creature_movement);
+DELETE FROM creature_movement WHERE id NOT IN (SELECT guid FROM creature);
+
+DELETE FROM creature_addon WHERE guid NOT IN (SELECT guid FROM creature);
+
+--UPDATE Database Version
+UPDATE db_version set version = 'ZeroDatabase 2.0.3 for MaNGOSZero zXXXX+ and ScriptDevZero zXXXX+';
