410a9961de779173da34fee5d5a08363a391d8b4
Salja <salja2012@hotmail.de>
2013-03-10 17:44:22 +0100

    [z0166] Quest CompleteScript for Quest (1063)
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/205_updatepack_mangos.sql
+++ _develop/205_updatepack_mangos.sql
@@ -711,6 +711,19 @@ INSERT INTO `db_script_string` (`entry`, `content_default`, `content_loc1`, `con
 ('2000000105','The spirits of Stonetalon still rage, $N.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 ('2000000106','I fear we may never soothe them...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 
+-- Quest CompleteScript for Quest (1063)
+UPDATE `quest_template` SET `CompleteScript` = 1063 WHERE `entry` = 1063;
+
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 1063;
+INSERT INTO `dbscripts_on_quest_end` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
+('1063','0','0','2','0','0','0','0','2000000108','0','0','0','0','0','0','0',''),
+('1063','2','0','0','0','0','0','0','2000000109','0','0','0','0','0','0','0','');
+
+DELETE FROM `db_script_string` WHERE `entry` IN (2000000108, 2000000109);
+INSERT INTO `db_script_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES
+('2000000108','%s listens to $N\'s plea.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+('2000000109','You were right to come. My wisdom will be yours.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
 -- Cleanup
 UPDATE `gameobject` SET `state` = 0 WHERE `id` IN (SELECT `entry` FROM `gameobject_template` WHERE `type` = 0 AND `data0` = 1);
 UPDATE `creature_template` SET `unit_flags` = `unit_flags` &~ 2048 WHERE `unit_flags` & 2048 = 2048;
