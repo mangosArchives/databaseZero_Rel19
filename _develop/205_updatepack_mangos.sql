12bdba82ea7ee3ed47f05f1c16a75dc6dfe5f47c
Salja <salja2012@hotmail.de>
2013-03-10 16:44:09 +0100

    [z0148] Quest StartScript for Quest (434)
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/205_updatepack_mangos.sql
+++ _develop/205_updatepack_mangos.sql
@@ -447,6 +447,13 @@ DELETE FROM `dbscripts_on_quest_start` WHERE `id` = 8305;
 INSERT INTO `dbscripts_on_quest_start` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
 ('8305','5','15','25201','0','0','0','0','0','0','0','0','0','0','0','0','');
 
+-- Quest StartScript for Quest (434)
+UPDATE `quest_template` SET `StartScript` = 434 WHERE `entry` = 434;
+
+DELETE FROM `dbscripts_on_quest_start` WHERE `id` = 434;
+INSERT INTO `dbscripts_on_quest_start` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
+('434','45','7','434','0','0','0','0','0','0','0','0','0','0','0','0','');
+
 -- Cleanup
 UPDATE `gameobject` SET `state` = 0 WHERE `id` IN (SELECT `entry` FROM `gameobject_template` WHERE `type` = 0 AND `data0` = 1);
 UPDATE `creature_template` SET `unit_flags` = `unit_flags` &~ 2048 WHERE `unit_flags` & 2048 = 2048;
