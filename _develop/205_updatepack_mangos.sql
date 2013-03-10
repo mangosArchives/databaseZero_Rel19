ebf46318eea30cdf6175090f6b68e920a334b4f0
Salja <salja2012@hotmail.de>
2013-03-10 16:46:27 +0100

    [z0153] Quest StartScript for Quest (1221)
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/205_updatepack_mangos.sql
+++ _develop/205_updatepack_mangos.sql
@@ -505,6 +505,22 @@ INSERT INTO `db_script_string` (`entry`, `content_default`, `content_loc1`, `con
 ('2000005015','Terenthis begins to cast a spell as you take the cloak from him.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 ('2000005016','Go now, $N. Find Volcor... and remember, the magics protecting you will fail if you interact with anyone, save for talking.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 
+-- Quest StartScript for Quest (1221)
+UPDATE `quest_template` SET `StartScript` = 1221 WHERE `entry` = 1221;
+
+DELETE FROM `dbscripts_on_quest_start` WHERE `id` = 1221;
+INSERT INTO `dbscripts_on_quest_start` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
+('1221','0','0','0','0','0','0','0','2000000107','0','0','0','0','0','0','0',''),
+('1221','0','1','3','0','0','0','0','0','0','0','0','0','0','0','0',''),
+('1221','2','3','0','0','0','0','0','0','0','0','0','-929.023','-3696.98','7.97395','1.7352',''),
+('1221','5','1','25','0','0','0','0','0','0','0','0','0','0','0','0',''),
+('1221','8','3','0','0','0','0','0','0','0','0','0','-928.778','-3697.25','7.99705','6.17741',''),
+('1221','9','3','0','0','0','0','0','0','0','0','0','-928.982','-3697.23','8.06642','3.08923','');
+
+DELETE FROM `db_script_string` WHERE `entry` = 2000000107;
+INSERT INTO `db_script_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES
+('2000000107','Hey $N! Don\'t forget to grab a crate, a manual and a command stick!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
 -- Cleanup
 UPDATE `gameobject` SET `state` = 0 WHERE `id` IN (SELECT `entry` FROM `gameobject_template` WHERE `type` = 0 AND `data0` = 1);
 UPDATE `creature_template` SET `unit_flags` = `unit_flags` &~ 2048 WHERE `unit_flags` & 2048 = 2048;
