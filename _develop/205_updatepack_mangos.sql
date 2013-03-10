23b3947120e32769839c62c10d1ab5e198259197
Salja <salja2012@hotmail.de>
2013-03-10 17:50:00 +0100

    [z0167] Quest CompleteScript for Quest (1066)
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/205_updatepack_mangos.sql
+++ _develop/205_updatepack_mangos.sql
@@ -724,6 +724,19 @@ INSERT INTO `db_script_string` (`entry`, `content_default`, `content_loc1`, `con
 ('2000000108','%s listens to $N\'s plea.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 ('2000000109','You were right to come. My wisdom will be yours.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 
+-- Quest CompleteScript for Quest (1066)
+UPDATE `quest_template` SET `CompleteScript` = 1066 WHERE `entry` = 1066;
+
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 1066;
+INSERT INTO `dbscripts_on_quest_end` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
+('1066','0','0','2','0','0','0','0','2000000110','0','0','0','0','0','0','0',''),
+('1066','4','0','0','0','0','0','0','2000000111','0','0','0','0','0','0','0','');
+
+DELETE FROM `db_script_string` WHERE `entry` IN (2000000110, 2000000111);
+INSERT INTO `db_script_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES
+('2000000110','%s creates his toxin...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+('2000000111','There we are, $N. The toxin is ready.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
 -- Cleanup
 UPDATE `gameobject` SET `state` = 0 WHERE `id` IN (SELECT `entry` FROM `gameobject_template` WHERE `type` = 0 AND `data0` = 1);
 UPDATE `creature_template` SET `unit_flags` = `unit_flags` &~ 2048 WHERE `unit_flags` & 2048 = 2048;
