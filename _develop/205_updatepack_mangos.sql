94fc7e1cdaf5c9048f860dd79d496e638532b74d
Salja <salja2012@hotmail.de>
2013-03-10 17:39:23 +0100

    [z0165] Quest CompleteScript for Quest (1062)
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/205_updatepack_mangos.sql
+++ _develop/205_updatepack_mangos.sql
@@ -698,6 +698,19 @@ INSERT INTO `db_script_string` (`entry`, `content_default`, `content_loc1`, `con
 ('2000005393','Grimclaw roars in acknowledgement at Terenthis.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 ('2000005394','If you have the time, Grimclaw and his master Volcor could use your help. If you\'re interested, speak with me further...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 
+-- Quest CompleteScript for Quest (1062)
+UPDATE `quest_template` SET `CompleteScript` = 1062 WHERE `entry` = 1062;
+
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 1062;
+INSERT INTO `dbscripts_on_quest_end` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
+('1062','4','0','0','0','0','0','0','2000000106','0','0','0','0','0','0','0',''),
+('1062','1','0','0','0','0','0','0','2000000105','0','0','0','0','0','0','0','');
+
+DELETE FROM `db_script_string` WHERE `entry` IN (2000000105, 2000000106);
+INSERT INTO `db_script_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES
+('2000000105','The spirits of Stonetalon still rage, $N.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+('2000000106','I fear we may never soothe them...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
 -- Cleanup
 UPDATE `gameobject` SET `state` = 0 WHERE `id` IN (SELECT `entry` FROM `gameobject_template` WHERE `type` = 0 AND `data0` = 1);
 UPDATE `creature_template` SET `unit_flags` = `unit_flags` &~ 2048 WHERE `unit_flags` & 2048 = 2048;
