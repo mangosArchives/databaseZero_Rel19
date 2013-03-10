674f663fe0abd41b89ee8ff97889600f0c4a6e90
Salja <salja2012@hotmail.de>
2013-03-10 17:17:35 +0100

    [z0156] Quest CompleteScript for Quest (630)
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/205_updatepack_mangos.sql
+++ _develop/205_updatepack_mangos.sql
@@ -547,6 +547,18 @@ INSERT INTO `db_script_string` (`entry`, `content_default`, `content_loc1`, `con
 ('2000000130','Thank you, $N.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 ('2000000131','I\'m sure I can find a use for this mixture...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 
+-- Quest CompleteScript for Quest (630)
+UPDATE `quest_template` SET `CompleteScript` = 630 WHERE `entry` = 630;
+
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 630;
+INSERT INTO `dbscripts_on_quest_end` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
+('630','2','1','3','0','0','0','0','0','0','0','0','0','0','0','0',''),
+('630','5','0','0','0','0','0','0','2000000134','0','0','0','0','0','0','0','');
+
+DELETE FROM `db_script_string` WHERE `entry` = 2000000134;
+INSERT INTO `db_script_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES
+('2000000134','Thank you again for your aid. May the wind always be at your back during your travels.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
 -- Cleanup
 UPDATE `gameobject` SET `state` = 0 WHERE `id` IN (SELECT `entry` FROM `gameobject_template` WHERE `type` = 0 AND `data0` = 1);
 UPDATE `creature_template` SET `unit_flags` = `unit_flags` &~ 2048 WHERE `unit_flags` & 2048 = 2048;
