dc12a7a5a973c877979e22b198972c16af8fa1da
Salja <salja2012@hotmail.de>
2013-03-10 17:16:45 +0100

    [z0154] Quest CompleteScript for Quest (609)
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/205_updatepack_mangos.sql
+++ _develop/205_updatepack_mangos.sql
@@ -521,6 +521,19 @@ DELETE FROM `db_script_string` WHERE `entry` = 2000000107;
 INSERT INTO `db_script_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES
 ('2000000107','Hey $N! Don\'t forget to grab a crate, a manual and a command stick!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 
+-- Quest CompleteScript for Quest (609)
+UPDATE `quest_template` SET `CompleteScript` = 609 WHERE `entry` = 609;
+
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 609;
+INSERT INTO `dbscripts_on_quest_end` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
+('609','2','0','2','0','0','0','0','2000000132','0','0','0','0','0','0','0',''),
+('609','5','0','0','0','0','0','0','2000000133','0','0','0','0','0','0','0','');
+
+DELETE FROM `db_script_string` WHERE `entry` IN (2000000132, 2000000133);
+INSERT INTO `db_script_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES
+('2000000132','%s tries opening Maury\'s Clubbed Foot...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+('2000000133','Bah! $N, this foot won\'t budge!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
 -- Cleanup
 UPDATE `gameobject` SET `state` = 0 WHERE `id` IN (SELECT `entry` FROM `gameobject_template` WHERE `type` = 0 AND `data0` = 1);
 UPDATE `creature_template` SET `unit_flags` = `unit_flags` &~ 2048 WHERE `unit_flags` & 2048 = 2048;
