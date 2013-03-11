6c845b340bd334ffd31edcfcd0ec8b9f154caa8d
Salja <salja2012@hotmail.de>
2013-03-11 11:10:29 +0100

    [z0187] Add gossip_menu for gameobject (20923) Thanks Ghurok (about the text i must check first)
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/206_updatepack_mangos.sql
+++ _develop/206_updatepack_mangos.sql
@@ -17,6 +17,11 @@
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+-- Add gossip_menu for gameobject (20923) Thanks Ghurok (about the text i must check first)
+DELETE FROM `gossip_menu` WHERE `entry` = 5004;
+INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES
+('5004','6062','0','0');
+
 -- Cleanup
 UPDATE `gameobject` SET `state` = 0 WHERE `id` IN (SELECT `entry` FROM `gameobject_template` WHERE `type` = 0 AND `data0` = 1);
 UPDATE `creature_template` SET `unit_flags` = `unit_flags` &~ 2048 WHERE `unit_flags` & 2048 = 2048;
