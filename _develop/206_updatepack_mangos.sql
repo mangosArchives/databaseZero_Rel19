234cf54a32be64a89cdee51c56cc85075e2994de
Evildead666 <Master_R_U@gmx.de>
2013-03-26 22:15:54 +0100

    Breaking the Keystone

--- _develop/206_updatepack_mangos.sql
+++ _develop/206_updatepack_mangos.sql
@@ -17,6 +17,12 @@
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+-- Added spawn and attack script for Thenan when completing quest 562 Breaking the keystone
+UPDATE `quest_template` SET `CompleteScript` = 652 WHERE `entry` = 652;
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 652;
+INSERT INTO `dbscripts_on_quest_end` VALUES (652,1,10,2763,180000,0,0,0,0,0,0,0,-1539.12,-2172.93,17.29,0.56,'Summon Thenan');
+INSERT INTO `dbscripts_on_quest_end` VALUES (652,3,26,0,0,2763,50,0,0,0,0,0,0,0,0,0,'Start Attack');
+
 -- fixing Quest 1388 previous questid, because quest where available before completing the first two quest of this questline.
 UPDATE `quest_template` SET `PrevQuestId` = 1383 WHERE `entry` = 1388;
 
