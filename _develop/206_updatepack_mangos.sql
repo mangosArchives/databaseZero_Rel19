a15f4f7f5d7948c35910b24c283952e11ca0f17f
Evildead666 <Master_R_U@gmx.de>
2013-03-29 14:28:45 +0100

    To the Top

--- _develop/206_updatepack_mangos.sql
+++ _develop/206_updatepack_mangos.sql
@@ -17,6 +17,13 @@
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+-- fixing Quest 3567 this is removing the "You learn spell" from complete quest window and spell gets casted by Marli not by player
+UPDATE `quest_template` SET `RewSpellCast` = 0 WHERE `entry` = 3567;
+UPDATE `quest_template` SET `RewSpell` = 0 WHERE `entry` = 3567;
+UPDATE `quest_template` SET `CompleteScript` = 3567 WHERE `entry` = 3567;
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 3567;
+INSERT INTO `dbscripts_on_quest_end` VALUES (3567,0,15,12242,0,0,0,0,0,0,0,0,0,0,0,0,'Cast spell 12242 on Player');
+
 -- Added Spellspcripts for Spells 11885, 11886, 1887, 11888, 11889 Despawning the corpse after use of the muisek vessel
 DELETE FROM `dbscripts_on_spell` WHERE `id` = 11885;
 INSERT INTO `dbscripts_on_spell` VALUES (11885,3,18,0,0,0,0,0,0,0,0,0,0,0,0,0,'Despawn treant corpse');
