e9b3c631fc390a1aa0588d0ec301471719b6d9b0
Evildead666 <Master_R_U@gmx.de>
2013-03-29 21:29:56 +0100

    Dalar Dawnweaver

--- _develop/206_updatepack_mangos.sql
+++ _develop/206_updatepack_mangos.sql
@@ -17,6 +17,32 @@
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+-- fixing the "you learn spell" from quest windows, from 6 Quest of Dalar Dawnweaver, adding quest end scripts so dalar dawnweaver cast spell on Player, fixing the not enough mana bug.
+UPDATE `quest_template` SET `RewSpellCast` = 0 WHERE `entry` = 99;
+UPDATE `quest_template` SET `RewSpellCast` = 0 WHERE `entry` = 421;
+UPDATE `quest_template` SET `RewSpellCast` = 0 WHERE `entry` = 422;
+UPDATE `quest_template` SET `RewSpellCast` = 0 WHERE `entry` = 423;
+UPDATE `quest_template` SET `RewSpellCast` = 0 WHERE `entry` = 424;
+UPDATE `quest_template` SET `RewSpellCast` = 0 WHERE `entry` = 1014;
+UPDATE `quest_template` SET `CompleteScript` = 99 WHERE `entry` = 99;
+UPDATE `quest_template` SET `CompleteScript` = 421 WHERE `entry` = 421;
+UPDATE `quest_template` SET `CompleteScript` = 422 WHERE `entry` = 422;
+UPDATE `quest_template` SET `CompleteScript` = 423 WHERE `entry` = 423;
+UPDATE `quest_template` SET `CompleteScript` = 424 WHERE `entry` = 424;
+UPDATE `quest_template` SET `CompleteScript` = 1014 WHERE `entry` = 1014;
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 99;
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 421;
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 422;
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 423;
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 424;
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 1014;
+INSERT INTO `dbscripts_on_quest_end` VALUES (99,0,15,8097,0,0,0,0,0,0,0,0,0,0,0,0,'Cast spell Arcane Intellect on Player');
+INSERT INTO `dbscripts_on_quest_end` VALUES (421,0,15,8097,0,0,0,0,0,0,0,0,0,0,0,0,'Cast spell Arcane Intellect on Player');
+INSERT INTO `dbscripts_on_quest_end` VALUES (422,0,15,8097,0,0,0,0,0,0,0,0,0,0,0,0,'Cast spell Arcane Intellect on Player');
+INSERT INTO `dbscripts_on_quest_end` VALUES (423,0,15,8097,0,0,0,0,0,0,0,0,0,0,0,0,'Cast spell Arcane Intellect on Player');
+INSERT INTO `dbscripts_on_quest_end` VALUES (424,0,15,8097,0,0,0,0,0,0,0,0,0,0,0,0,'Cast spell Arcane Intellect on Player');
+INSERT INTO `dbscripts_on_quest_end` VALUES (1014,0,15,8097,0,0,0,0,0,0,0,0,0,0,0,0,'Cast spell Arcane Intellect on Player');
+
 -- fixing Quest 3567 this is removing the "You learn spell" from complete quest window and spell gets casted by Marli not by player
 UPDATE `quest_template` SET `RewSpellCast` = 0 WHERE `entry` = 3567;
 UPDATE `quest_template` SET `RewSpell` = 0 WHERE `entry` = 3567;
