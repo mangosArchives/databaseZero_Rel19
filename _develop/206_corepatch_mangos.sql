fba044c3cdf33e6653448fcc8604bd6153247959
Salja <salja2012@hotmail.de>
2013-03-30 14:41:09 +0100

    [z2054] scriptdev2 update
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/206_corepatch_mangos.sql
+++ _develop/206_corepatch_mangos.sql
@@ -17,6 +17,7 @@
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+-- z2302
 ALTER TABLE db_version CHANGE COLUMN required_z2268_s1900_12370_02_mangos_npc_vendor_template required_z2302_sxxxx_xxxxx_01_mangos_string bit;
 
 DELETE FROM mangos_string WHERE entry =548;
@@ -24,6 +25,7 @@ DELETE FROM mangos_string WHERE entry =548;
 INSERT INTO mangos_string VALUES
 (548,'Player%s %s (guid: %u) Account: %s (id: %u) GMLevel: %u E-mail: %s Last IP: %s Last login: %s Latency: %ums',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 
+-- z2330
 ALTER TABLE db_version CHANGE COLUMN required_z2302_sxxxx_xxxxx_01_mangos_string required_z2330_sxxxx_xxxxx_01_mangos_spell_proc_event bit;
 
 DELETE FROM `spell_proc_event` WHERE `entry` = 23401;
--- _develop/206_updatepack_mangos.sql
+++ _develop/206_updatepack_mangos.sql
@@ -490,7 +490,10 @@ INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`) VALUES
 UPDATE `quest_template` SET `ReqCreatureorGOId1` = 13017 WHERE `entry` = 6661;
 
 -- ScriptDev2
+-- z2647
 UPDATE `creature_template` SET ScriptName='spell_dummy_npc' WHERE `entry` = 13016;
+-- z2649
+UPDATE gameobject_template SET ScriptName='go_father_flame' WHERE entry=175245;
 
 -- Cleanup
 UPDATE `gameobject` SET `state` = 0 WHERE `id` IN (SELECT `entry` FROM `gameobject_template` WHERE `type` = 0 AND `data0` = 1);
