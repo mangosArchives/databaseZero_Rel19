d72a5d16d3c9692ebec2972376ab62dc4ca1d7f8
Titarion <titarion@gmail.com>
2013-04-06 18:53:12 -0300

    [z0286] Changes to Quest: Identifying the Brood

--- _develop/206_updatepack_mangos.sql
+++ _develop/206_updatepack_mangos.sql
@@ -17,6 +17,17 @@
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+/* Changes to the Quest: Identifying the Brood (1169):
+   Added this Quest to it's proper chain
+   Dropped the Min. Level requirement for all Quests in this chain to 38, down from 39
+   Players must now obtain 15 items of each instead of 7
+
+   Sources: http://web.archive.org/web/20070216194659/http://www.wowwiki.com/Quest:Identifying_the_Brood
+            http://www.wowhead.com/quest=1169/deprecated-identifying-the-brood#comments:id=183809
+*/
+UPDATE `quest_template` SET `Objectives` = 'Draz\'Zilb in Brackenwall Village would like you to bring him 15 Searing Tongues and 15 Searing Hearts.', `ReqItemCount1` = 15, `ReqItemCount2` = 15, `NextQuestInChain` = 1170 WHERE `entry` = 1169;
+UPDATE `quest_template` SET `MinLevel` = 38 WHERE `entry` IN (1170, 1171, 1172);
+
 /* Structure of npc_text:
 (`ID`, `text0_0`, `text0_1`, `lang0`, `prob0`, `em0_0`, `em0_1`, `em0_2`, `em0_3`, `em0_4`, `em0_5`, `text1_0`, `text1_1`, `lang1`, `prob1`, `em1_0`, `em1_1`, `em1_2`, `em1_3`, `em1_4`, `em1_5`, `text2_0`, `text2_1`, `lang2`, `prob2`, `em2_0`, `em2_1`, `em2_2`, `em2_3`, `em2_4`, `em2_5`, `text3_0`, `text3_1`, `lang3`, `prob3`, `em3_0`, `em3_1`, `em3_2`, `em3_3`, `em3_4`, `em3_5`, `text4_0`, `text4_1`, `lang4`, `prob4`, `em4_0`, `em4_1`, `em4_2`, `em4_3`, `em4_4`, `em4_5`, `text5_0`, `text5_1`, `lang5`, `prob5`, `em5_0`, `em5_1`, `em5_2`, `em5_3`, `em5_4`, `em5_5`, `text6_0`, `text6_1`, `lang6`, `prob6`, `em6_0`, `em6_1`, `em6_2`, `em6_3`, `em6_4`, `em6_5`, `text7_0`, `text7_1`, `lang7`, `prob7`, `em7_0`, `em7_1`, `em7_2`, `em7_3`, `em7_4`, `em7_5`)
 */
