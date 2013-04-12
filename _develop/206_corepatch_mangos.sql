c2a4b25124a8ca7cc31c11bec712254c4ee2b10e
Salja <salja2012@hotmail.de>
2013-04-13 00:12:58 +0200

    [z0323] corepatch for next update
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/206_corepatch_mangos.sql
+++ _develop/206_corepatch_mangos.sql
@@ -31,3 +31,8 @@ ALTER TABLE db_version CHANGE COLUMN required_z2302_sxxxx_xxxxx_01_mangos_string
 DELETE FROM `spell_proc_event` WHERE `entry` = 23401;
 INSERT INTO spell_proc_event VALUES
 (23401, 0x00,  6, 0x0000000000041e00, 0x0000000000000000, 0x0000000000000000, 0x00004000, 0x00000000, 0.000000, 0.000000,  0);
+
+-- z2363
+ALTER TABLE db_version CHANGE COLUMN required_z2330_sxxxx_xxxxx_01_mangos_spell_proc_event required_z2363_s1957_12440_01_mangos_spell_area bit;
+
+ALTER TABLE spell_area ADD COLUMN `condition_id` mediumint(8) unsigned NOT NULL default '0' AFTER quest_end;
