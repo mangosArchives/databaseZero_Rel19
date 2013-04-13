4ddae4b2268ceb2d698b6ceb7961ef28090acf27
Titarion <titarion2@gmail.com>
2013-04-13 12:53:26 -0300

    A few updates

--- _develop/_developer_files/titarion.sql
+++ _develop/_developer_files/titarion.sql
@@ -1,8 +1,34 @@
--- titarion
+/* titarion
+- Removed Mana Points from all Rogue Trainers.
+- Significantly reduced the amount of Webwood Spiders in the Shadowthread Cave.
+- Added randomized movement to a few Webwood Spiders and to Githyiss the Vile.
+- Added the GameObject 'Naxxramas'. It can now be seen floating above Plaguewood.
+*/
 
 -- creature
+/* Removed Mana Points from all Rogue Trainers.
+   Sources: http://www.youtube.com/watch?v=ZFuaZVM_5Ik
+            http://www.wowpedia.org/index.php?title=Rogue_trainers&oldid=410531
+*/
+UPDATE `creature` SET `curmana` = 0 WHERE `id` IN (915, 916, 917, 918, 1234, 1411, 2122, 2130, 3155, 3170, 3327, 3328, 3401, 3594, 3599, 4163, 4214, 4215, 4582, 4583, 4584, 5165, 5166, 5167, 13283);    
+UPDATE `creature_template` SET `minmana` = 0, `maxmana` = 0 WHERE `subname` = 'Rogue Trainer';
+
+/* Significantly reduced the amount of Webwood Spiders in the Shadowthread Cave.
+   Added randomized movement to a few Webwood Spiders and to Githyiss the Vile.
+   Source: http://www.youtube.com/watch?v=eqA2R8gsmPA&t=4m33s
+*/
+DELETE FROM `creature` WHERE `guid` IN (47013, 47061, 47015, 47016, 47017,  47262, 47263, 46996, 47004, 47005, 47007, 47030, 47031);
+DELETE FROM `creature_addon` WHERE `guid` IN (47013, 47061, 47015, 47016, 47017,  47262, 47263, 46996, 47004, 47005, 47007, 47030, 47031);
+UPDATE `creature` SET `MovementType` = 1, `spawndist` = 5 WHERE `guid` IN (47058, 47018, 47029, 47056, 47057, 47059, 47006, 47350);
 
 -- gameobject
+/* Added the GameObject 'Naxxramas'. It can now be seen floating above Plaguewood.
+   Sources: http://www.wowwiki.com/File:Naxxramas-1.jpg
+            Turtleswin
+*/
+DELETE FROM `gameobject` WHERE `id` = 181056;
+INSERT INTO `gameobject` VALUES
+(45843, 181056, 0, 3157.28, -3743.47, 236.951, 2.76712, 0, 0, 0.982523, 0.186143, 25, 100, 1);
 
 -- items
 
