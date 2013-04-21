de362ef15593cce8713688c2da1edfeefcf61be3
Anthony O'Dell <antz@cix.co.uk>
2013-04-21 21:15:08 +0100

    cleaned up batch file

--- _tools/MaNGOSdb_Installer.bat
+++ _tools/MaNGOSdb_Installer.bat
@@ -1,25 +1,24 @@
 @echo off
 :quick
 rem Quick install section
-rem This will automatically use the variables below to install the world and scriptdev2 databases without prompting then optimize them and exit
+rem This will automatically use the variables below to install the world databases without prompting then optimize them and exit
 rem To use: Set your environment variables below and change 'set quick=off' to 'set quick=on' 
 set quick=off
 if %quick% == off goto standard
 echo (( Mangos World Database Quick Installer ))
 rem -- Change the values below to match your server --
 set svr=localhost
-set user=root
+set user=mangos
 set pass=
 set port=3306
 set wdb=mangos
-set sd2db=scriptdev2
 rem -- Don't change past this point --
 set yesno=y
 goto install
 
 :standard
 rem Standard install section
-color 3
+color 3B
 echo .
 echo MM   MM         MM   MM  MMMMM   MMMM   MMMMM
 echo MM   MM         MM   MM MMM MMM MM  MM MMM MMM
@@ -35,11 +34,13 @@ echo         MMMMMM
 echo .
 
 echo Credits to: Factionwars, Nemok, BrainDedd and Antz
-
+color 02
+echo ==================================================
+echo .
 set /p svr=What is your MySQL host name?           [localhost]   : 
 if %svr%. == . set svr=localhost
-set /p user=What is your MySQL user name?           [root]        : 
-if %user%. == . set user=root
+set /p user=What is your MySQL user name?           [mangos]      : 
+if %user%. == . set user=mangos
 set /p pass=What is your MySQL password?            [ ]           : 
 if %pass%. == . set pass=
 set /p port=What is your MySQL port?                [3306]        : 
@@ -69,7 +70,7 @@ if %quick% == off set /p yesno=Do you wish to continue? (y/n)
 echo.
 echo Importing World database
 
-for %%i in (%dbpath%\*.sql) do if %%i neq %dbpath%\scriptdev2.sql if %%i neq %dbpath%\characters.sql if %%i neq %dbpath%\realmd.sql echo %%i & %mysql%\mysql -q -s -h %svr% --user=%user% --password=%pass% --port=%port% %wdb% < %%i
+for %%i in (%dbpath%\*.sql) do echo %%i & %mysql%\mysql -q -s -h %svr% --user=%user% --password=%pass% --port=%port% %wdb% < %%i
 
 :done
 echo.
