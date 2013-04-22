b8196a7fd792a3b52b0589b187f9e487aec4d79d
Salja <salja2012@hotmail.de>
2013-04-22 14:33:18 +0200

    - Uldaman missing chest UDB 406
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/_developer_files/salja.sql
+++ _develop/_developer_files/salja.sql
@@ -11,6 +11,11 @@ INSERT INTO gameobject (guid, id, map, position_x, position_y, position_z, orien
 (94156, 180691, 509, -8938.357, 1860.11328, 3.230413, -2.530723, 0, 0, -0.9537163, 0.3007079, 7200, 100, 1),
 (94157, 180691, 509, -9090.657, 1500.95874, 24.4341373, 0.9773831, 0, 0, 0.469470978, 0.8829479, 7200, 100, 1);
 
+-- Uldaman missing chest UDB 406
+DELETE FROM gameobject WHERE guid = 94154;
+INSERT INTO gameobject (guid, id, map, position_x, position_y, position_z, orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecs, animprogress, state) VALUES
+(94154, 141979, 70, 153.39, 289.091, -52.2262, 2.68781, 0, 0, 0, 0, -14400, 255, 1);
+
 -- items
 
 -- quests
