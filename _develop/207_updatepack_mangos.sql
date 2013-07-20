390569c4b09ed2fa0c72f44f08ffb1949ee68c0f
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-07-20 18:15:26 +0200

    - Added proper graveyard connections for Blackrock mountains and related zones.
    Thanks to [deskicio](http://community.getmangos.co.uk/index.php?action=profile;u=72288) for spotting the issue.
    
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- _develop/207_updatepack_mangos.sql
+++ _develop/207_updatepack_mangos.sql
@@ -199,6 +199,28 @@ INSERT INTO `dbscripts_on_quest_end` VALUES (423,0,15,8097,0,0,0,0,0,0,0,0,0,0,0
 INSERT INTO `dbscripts_on_quest_end` VALUES (424,0,15,8097,0,0,0,0,0,0,0,0,0,0,0,0,'Cast spell Arcane Intellect on Player');
 INSERT INTO `dbscripts_on_quest_end` VALUES (1014,0,15,8097,0,0,0,0,0,0,0,0,0,0,0,0,'Cast spell Arcane Intellect on Player');
 
+-- Replace graveyard for Blackrock mountains and related zones with Throrium point
+SET @FACTION_ANY                                = 0;
+SET @FACTION_ALLIANCE                           = 469;
+SET @FACTION_HORDE                              = 67;
+SET @ZONE_BLACKROCK_DEPTHS                      = 1584;
+SET @ZONE_BLACKROCK_MOUNTAIN                    = 25;
+SET @ZONE_BLACKROCK_SPIRE                       = 1583;
+SET @ZONE_MOLTEN_CORE                           = 2717;
+SET @ZONE_BLACKWING_LAIR                        = 2677;
+
+-- Remove old graveyard links for Blackrock mountains and related zones
+DELETE FROM `game_graveyard_zone`
+WHERE `ghost_zone` IN (@ZONE_BLACKROCK_DEPTHS, @ZONE_BLACKROCK_MOUNTAIN, @ZONE_BLACKROCK_SPIRE, @ZONE_MOLTEN_CORE, @ZONE_BLACKWING_LAIR);
+
+-- Add proper graveyards for Blackrock mountains and related zones
+INSERT INTO `game_graveyard_zone` VALUES
+    (636,   @ZONE_BLACKROCK_DEPTHS,             @FACTION_ANY),
+    (636,   @ZONE_BLACKROCK_MOUNTAIN,           @FACTION_ANY),
+    (636,   @ZONE_BLACKROCK_SPIRE,              @FACTION_ANY),
+    (636,   @ZONE_MOLTEN_CORE,                  @FACTION_ANY),
+    (636,   @ZONE_BLACKWING_LAIR,               @FACTION_ANY);
+
 -- ACID
 
 -- ScriptDev2
