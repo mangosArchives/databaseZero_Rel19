305fdd5ea5b22c9c592f9b2e955943d072d59002
Evildead666 <Master_R_U@gmx.de>
2013-07-20 18:02:25 +0200

    quest end script 524 Elixir of Agony

--- _develop/207_updatepack_mangos.sql
+++ _develop/207_updatepack_mangos.sql
@@ -17,6 +17,22 @@
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+-- Quest end script for Quest 524 Elixir Of Agony
+UPDATE `creature` SET `spawntimesecs` = 180 WHERE `id` = 2284;
+UPDATE `gameobject` SET `spawntimesecs` = -10 WHERE `guid` = 30031;
+DELETE FROM `gameobject` WHERE `guid` = 55530;
+INSERT INTO `gameobject` VALUES (55530,1730,0,0.437117,-942.794,61.9384,-2.54818,0,0,0,0,-10,100,1);
+UPDATE `quest_template` SET `CompleteScript` = 524 WHERE `entry` = 524;
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 524;
+INSERT INTO `dbscripts_on_quest_end` VALUES (524,1,9,30031,30,0,0,0,0,0,0,0,0,0,0,0,'Spawn Keg');
+INSERT INTO `dbscripts_on_quest_end` VALUES (524,2,9,55530,30,0,0,0,0,0,0,0,0,0,0,0,'Spawn Keg Smoke');
+INSERT INTO `dbscripts_on_quest_end` VALUES (524,4,3,0,0,2284,15891,16,0,0,0,0,0.524,-944.41,61.93,1.66,'Move first');
+INSERT INTO `dbscripts_on_quest_end` VALUES (524,4,3,0,0,2284,15893,16,0,0,0,0,0.439,-940.84,61.93,4.94,'Move second');
+INSERT INTO `dbscripts_on_quest_end` VALUES (524,4,3,0,0,2284,15892,16,0,0,0,0,-1.632,-942.43,61.93,6.14,'Move third');
+INSERT INTO `dbscripts_on_quest_end` VALUES (524,7,15,5,0,2284,15891,17,0,0,0,0,0,0,0,0,'Kill first');
+INSERT INTO `dbscripts_on_quest_end` VALUES (524,8,15,5,0,2284,15893,17,0,0,0,0,0,0,0,0,'Kill second');
+INSERT INTO `dbscripts_on_quest_end` VALUES (524,9,15,5,0,2284,15892,17,0,0,0,0,0,0,0,0,'Kill third');
+
 -- correct all reputation values for tirisfal
 UPDATE `quest_template` SET `RewRepValue1` = 100 WHERE `entry` = 354;
 UPDATE `quest_template` SET `RewRepValue1` = 50 WHERE `entry` = 356;
