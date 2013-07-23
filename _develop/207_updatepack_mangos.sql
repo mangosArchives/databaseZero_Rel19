79d96da8535d9142ccab7082538381d8c052966b
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-07-23 09:18:25 +0200

    - Added Naxxramas weapon corrections/additions.
    Thanks to Ela.
    
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- _develop/207_updatepack_mangos.sql
+++ _develop/207_updatepack_mangos.sql
@@ -17,6 +17,107 @@
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+-- Added Weapons for Plagued Warrior
+-- Source: http://www.youtube.com/watch?v=cxch-KEATeQ
+-- Source: http://www.wowcr.net/videos/vid08-naxxramas/
+UPDATE `creature_template` SET `equipment_id` = 150 WHERE `entry` = 16982;
+
+-- Added Weapons for Plagued Warrior
+-- Source: http://www.youtube.com/watch?v=cxch-KEATeQ
+-- Source: http://www.wowcr.net/videos/vid08-naxxramas/
+UPDATE `creature_template` SET `equipment_id` = 1872 WHERE `entry` = 16984;
+
+-- Added Weapons for Plagued Champion
+-- Source: http://www.youtube.com/watch?v=cxch-KEATeQ
+-- Source: http://www.wowcr.net/videos/vid08-naxxramas/
+UPDATE `creature_template` SET `equipment_id` = 149 WHERE `entry` = 16983;
+
+-- Added Weapons for Plagued Guardian
+-- Source: http://www.wowcr.net/videos/vid08-naxxramas/
+-- Source: http://www.youtube.com/watch?v=cxch-KEATeQ
+UPDATE `creature_template` SET `equipment_id` = 788 WHERE `entry` = 16981;
+
+-- Added Correct Weapon and Equip Template Lady Blaumeux
+-- Source: http://www.wowhead.com/npc=16065#screenshots
+-- Source: http://www.wowwiki.com/Four_Horsemen_%28original%29?file=FourHorsemen.jpg
+-- Source: http://evowde.lima-city.de/forum/tactics/www.wow-tactics.de/gfx/naxx/ss-reiter.jpg
+DELETE FROM `creature_equip_template_raw` WHERE `entry` = 2406;
+INSERT INTO `creature_equip_template_raw` (`entry`, `equipmodel1`, `equipmodel2`, `equipmodel3`, `equipinfo1`, `equipinfo2`, `equipinfo3`, `equipslot1`, `equipslot2`, `equipslot3`) VALUES (2406, 35258, 0, 0, 13639426, 0, 0, 3, 3, 0);
+UPDATE `creature_template` SET `equipment_id` = 2406 WHERE `entry` = 16065;
+
+-- Added Correct Weapon and Equip Template for Sir Zeliek
+-- Source: http://www.wowhead.com/npc=16063#screenshots
+-- Source: http://www.wowwiki.com/Four_Horsemen_%28original%29?file=FourHorsemen.jpg
+-- Source: http://evowde.lima-city.de/forum/tactics/www.wow-tactics.de/gfx/naxx/ss-reiter.jpg
+DELETE FROM `creature_equip_template_raw` WHERE `entry` = 2405;
+INSERT INTO `creature_equip_template_raw` (`entry`, `equipmodel1`, `equipmodel2`, `equipmodel3`, `equipinfo1`, `equipinfo2`, `equipinfo3`, `equipslot1`, `equipslot2`, `equipslot3`) VALUES (2405, 35710, 0, 0, 13639426, 0, 0, 3, 3, 0);
+UPDATE `creature_template` SET `equipment_id` = 2405 WHERE `entry` = 16063;
+
+-- Added Weapons for Death Lord
+-- Source: http://www.youtube.com/watch?v=7VcozSUZc9Y
+-- Source: http://www.youtube.com/watch?v=KxITOD-fQ5g
+-- Source: http://www.youtube.com/watch?v=PQ88MOZ-gGk
+UPDATE `creature_template` SET `equipment_id` = 1268 WHERE `entry` = 16861;
+
+-- Added Weapons for Unrelenting rider
+-- Source: http://www.youtube.com/watch?v=FN2QtHx8_xw
+UPDATE `creature_template` SET `equipment_id` = 1232 WHERE `entry` = 16126;
+
+-- Added Weapons for Spectral Rider
+-- Source: http://www.youtube.com/watch?v=FN2QtHx8_xw
+UPDATE `creature_template` SET `equipment_id` = 1232 WHERE `entry` = 16150;
+
+-- Added Weapons for Deathknight Vindicator
+-- Source: http://www.wowhead.com/npc=16451#screenshots
+UPDATE `creature_template` SET `equipment_id` = 1232 WHERE `entry` = 16451;
+
+-- Added Weapons for Necropolis Acolyte
+-- Source: http://www.wowhead.com/npc=16368#screenshots
+UPDATE `creature_template` SET `equipment_id` = 954 WHERE `entry` = 16368;
+
+-- Added Weapons for Necro Knight Guardian
+-- Source: http://www.wowhead.com/npc=16452#screenshots
+UPDATE `creature_template` SET `equipment_id` = 1451 WHERE `entry` = 16452;
+
+-- Added Weapons and  Equip template for Necro Knight
+-- Source: http://www.wowhead.com/npc=16165#screenshots
+DELETE FROM `creature_equip_template_raw` WHERE `entry` = 2404;
+INSERT INTO `creature_equip_template_raw` (`entry`, `equipmodel1`, `equipmodel2`, `equipmodel3`, `equipinfo1`, `equipinfo2`, `equipinfo3`, `equipslot1`, `equipslot2`, `equipslot3`) VALUES (2404, 35712, 0, 0, 218171138, 0, 0, 3, 0, 0);
+UPDATE `creature_template` SET `equipment_id` = 2404 WHERE `entry` = 16165;
+
+-- Added Weapons and  Equip template for Thane Kor'thazz
+-- Source: http://www.wowhead.com/npc=16064#screenshots
+-- Source: http://evowde.lima-city.de/forum/tactics/www.wow-tactics.de/gfx/naxx/ss-reiter.jpg
+-- Source: http://www.wowwiki.com/Four_Horsemen_%28original%29?file=FourHorsemen.jpg
+DELETE FROM `creature_equip_template_raw` WHERE `entry` = 2403;
+INSERT INTO `creature_equip_template_raw` (`entry`, `equipmodel1`, `equipmodel2`, `equipmodel3`, `equipinfo1`, `equipinfo2`, `equipinfo3`, `equipslot1`, `equipslot2`, `equipslot3`) VALUES (2403, 35129, 0, 0, 218171138, 0, 0, 3, 0, 0);
+UPDATE `creature_template` SET `equipment_id` = 2403 WHERE `entry` = 16064;
+
+-- Added Weapons and  Equip template for Spectral/Unrelenting Deathknight
+-- Source: http://www.youtube.com/watch?v=FN2QtHx8_xw
+DELETE FROM `creature_equip_template_raw` WHERE `entry` = 2402;
+INSERT INTO `creature_equip_template_raw` (`entry`, `equipmodel1`, `equipmodel2`, `equipmodel3`, `equipinfo1`, `equipinfo2`, `equipinfo3`, `equipslot1`, `equipslot2`, `equipslot3`) VALUES (2402, 16128, 30993, 0, 218169346, 234948100, 0, 3, 4, 0);
+UPDATE `creature_template` SET `equipment_id` = 2402 WHERE `entry` = 16125;
+UPDATE `creature_template` SET `equipment_id` = 2402 WHERE `entry` = 16148;
+
+-- Added Weapons to Npc Unholy Staff.
+-- Source:http://www.wowhead.com/npc=16215#screenshots
+-- Source: http://pacifistguild.org/index.cgi?start=24
+UPDATE `creature_template` SET `equipment_id` = 1205 WHERE `entry` = 16215;
+
+-- Added Weapons  and equip template for Npc Unholy Swords.
+-- Source: http://www.wowhead.com/npc=16216#screenshots
+DELETE FROM `creature_equip_template_raw` WHERE `entry` = 2401;
+INSERT INTO `creature_equip_template_raw` (`entry`, `equipmodel1`, `equipmodel2`, `equipmodel3`, `equipinfo1`, `equipinfo2`, `equipinfo3`, `equipslot1`, `equipslot2`, `equipslot3`) VALUES (2401, 23798, 23798, 0, 13639426, 13639426, 0, 3, 3, 0);
+UPDATE `creature_template` SET `equipment_id` = 2401 WHERE `entry` = 16216;
+
+-- Added Weapons and Equip Template for Unholy Axe
+-- Source: http://www.wowhead.com/npc=16194#screenshots
+-- Source: http://pacifistguild.org/index.cgi?start=24
+DELETE FROM `creature_equip_template_raw` WHERE `entry` = 2400;
+INSERT INTO `creature_equip_template_raw` (`entry`, `equipmodel1`, `equipmodel2`, `equipmodel3`, `equipinfo1`, `equipinfo2`, `equipinfo3`, `equipslot1`, `equipslot2`, `equipslot3`) VALUES (2400, 21238, 21238, 0, 13639426, 13639426, 0, 3, 3, 0);
+UPDATE `creature_template` SET `equipment_id` = 2400 WHERE `entry` = 16194;
+
 -- Add gossip to npc (3144) for quest (4941)
 SET @CONDITION_ENTRY                          = 718;
 SET @CONDITION_QUESTTAKEN                     = 9;
@@ -410,7 +511,7 @@ DELETE FROM `reference_loot_template` WHERE `entry`='34016';
 DELETE FROM `reference_loot_template` WHERE `entry`='34017';
 DELETE FROM `reference_loot_template` WHERE `entry`='34018';
 -- Create groups for the drops
-INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES 
+INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES
 -- Tier 2 pants reference 34016
 ('34016', '16901', '0', '1', '1', '1', '0'),
 ('34016', '16909', '0', '1', '1', '1', '0'),
@@ -442,7 +543,7 @@ DELETE FROM `creature_loot_template`
 WHERE `entry`='11502';
 
 -- Add new loot table
-INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES 
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES
 -- Essence of fire
 ('11502', '7078', '40', '0', '1', '9', '0'),
 -- Eye of sulfuras
