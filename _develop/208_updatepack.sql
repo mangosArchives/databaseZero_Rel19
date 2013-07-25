b661e045169aab8c287f612ecd7ca542564eb190
Evildead666 <Master_R_U@gmx.de>
2013-07-25 22:14:01 +0200

    quest end script for quest 808 Minshinas Skull

--- _develop/208_updatepack.sql
+++ _develop/208_updatepack.sql
@@ -17,6 +17,16 @@
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+-- scripted the quest end script for quest 808 Minshinas Skull
+UPDATE `creature_template` SET `MovementType` = 0 WHERE `entry` = 3289;
+UPDATE `quest_template` SET `CompleteScript` = 808 WHERE `entry` = 808;
+DELETE FROM `dbscripts_on_quest_end` WHERE `id` = 808;
+INSERT INTO `dbscripts_on_quest_end` VALUES (808,0,10,3289,20000,0,0,0,0,0,0,0,-823.88,-4924.51,19.71,1.89,'Summon Minshinas Ghost');
+INSERT INTO `dbscripts_on_quest_end` VALUES (808,1,0,0,0,0,0,0,2000000808,0,0,0,0,0,0,0,'Gadrin Say');
+INSERT INTO `dbscripts_on_quest_end` VALUES (808,3,1,2,0,3289,20,0,0,0,0,0,0,0,0,0,'Minshina Bow');
+DELETE FROM `db_script_string` WHERE `entry` = 2000000808;
+INSERT INTO `db_script_string` VALUES (2000000808,"I thank you. $n. And my brother thanks you.",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,NULL);
+
 -- Quest 842 Crossroads Conscription Offer Reward text fixed. removing $ sign
 UPDATE `quest_template` SET `OfferRewardText` = "Alright, $n. You want to earn your keep with the Horde? Well there's plenty to do here, so listen close and do what you're told.$B$B$GI see that look in your eyes, do not think I will tolerate any insolence. Thrall himself has declared the Hordes females to be on equal footing with you men. Disrespect me in the slightest, and you will know true pain.:I'm happy to have met you. Thrall will be glad to know that more females like you and I are taking the initiative to push forward in the Barrens.;" WHERE `entry` = 842;
 
