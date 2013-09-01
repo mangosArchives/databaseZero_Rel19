d9888ff1edf18ee78d1e7a5165bbfcad48d1c1cd
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-09-01 19:42:29 +0200

    - Corrected RewMoneyMaxLevel values.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- _develop/208_updatepack.sql
+++ _develop/208_updatepack.sql
@@ -3767,13 +3767,13 @@ UPDATE `creature_template` SET `rank` = 4 WHERE `entry` = 5933; -- Achellios the
 UPDATE `creature_template` SET `rank` = 4 WHERE `entry` = 7895; -- Ambassador Bloodrage, found in: THE BARRENS
 
 -- Fixes issue #3: correct experience rewarded for quest
-UPDATE `quest_template` SET `RewMoneyMaxLevel` = 2280 WHERE `entry` = 4402;
+UPDATE `quest_template` SET `RewMoneyMaxLevel` = 228 WHERE `entry` = 4402;
 
 -- Fixes issue #14: correct experience rewarded for quest
-UPDATE `quest_template` SET `RewMoneyMaxLevel` = 37200 WHERE `entry` = 8464;
+UPDATE `quest_template` SET `RewMoneyMaxLevel` = 3720 WHERE `entry` = 8464;
 
 -- Fixes issue #22: correct experience rewarded for quest.
-UPDATE `quest_template` SET `RewMoneyMaxLevel` = 26400 WHERE `entry` = 8460;
+UPDATE `quest_template` SET `RewMoneyMaxLevel` = 2640 WHERE `entry` = 8460;
 
 -- Fixes issue #9: added missing loot condition for quest item loot.
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`)
