d9f26093b85d6932b90e4bcbafea925dbbfa6533
Evildead666 <Master_R_U@gmx.de>
2013-11-07 20:57:05 +0100

    more old fixes

--- _develop/210_updatepack.sql
+++ _develop/210_updatepack.sql
@@ -17,6 +17,70 @@
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+-- Fixed quest 1846: item 7131 now is a 100% drop for characters on the quest.
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -100 WHERE `item` = 7131;
+
+-- Fixed creature 299: Young Wolf will ignore aggro now.
+UPDATE `creature_template` SET `flags_extra` = 2 WHERE `entry` = 299;
+
+-- Fixed quest 111: removed RequestItemsText.
+UPDATE `quest_template` SET `RequestItemsText` = '' WHERE `entry` = 111;
+
+-- Fixed quest 3065: restricted quest faction to Troll.
+UPDATE `quest_template` SET `RequiredRaces` = 128 WHERE `entry` = 3065;
+
+-- Fixed quest 5624: added punctuation to quest texts.
+UPDATE `quest_template` SET `OfferRewardText` = 'Excellent, $N. With one more healthy guard out there to help the town, we\'ll be that much safer. I\'m glad to see you\'re already learning to use your abilities wisely. If you feel you are ready for more training at any time, please come back to me. But for now, take this robe. It will let others know that you are one of our order. If you don\'t want to wear it, that\'s fine. Later there will be more tests and this robe isn\'t necessary for those.' WHERE `entry` = 5624;
+
+-- Fixed quest 5625: added punctuation to quest texts.
+UPDATE `quest_template` SET `OfferRewardText` = 'Excellent, $N. With one more healthy guard out there to help the town, we\'ll be that much safer. I\'m glad to see you\'re already learning to use your abilities wisely. If you feel you are ready for more training at any time, please come back to me. But for now, take this robe. It will let others know that you are one of our order. If you don\'t want to wear it, that\'s fine. Later there will be more tests and this robe isn\'t necessary for those.' WHERE `entry` = 5625;
+
+-- Fixed quest 6072: added OfferRewardText.
+UPDATE `quest_template` SET `OfferRewardText` = 'Yes, it is time, $c.' WHERE `entry` = 6072;
+
+-- Fixed quest 6126: removed PrevQuestId, thus the quest is now attainable.
+UPDATE `quest_template` SET `PrevQuestId` = 0 WHERE `entry` = 6126;
+
+-- Added on kill reputation for a few Ahn'Qiraj creatures. 
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15318;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15319;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15325;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15327;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15335;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15336;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15355;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15505;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15514;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15538;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15555;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15934;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 15741;
+INSERT INTO `creature_onkill_reputation` VALUES (15318,609,0,7,0,5,0,0,0,0);
+INSERT INTO `creature_onkill_reputation` VALUES (15319,609,0,7,0,5,0,0,0,0);
+INSERT INTO `creature_onkill_reputation` VALUES (15325,609,0,7,0,5,0,0,0,0);
+INSERT INTO `creature_onkill_reputation` VALUES (15327,609,0,7,0,5,0,0,0,0);
+INSERT INTO `creature_onkill_reputation` VALUES (15335,609,0,7,0,5,0,0,0,0);
+INSERT INTO `creature_onkill_reputation` VALUES (15336,609,0,7,0,5,0,0,0,0);
+INSERT INTO `creature_onkill_reputation` VALUES (15355,609,0,7,0,5,0,0,0,0);
+INSERT INTO `creature_onkill_reputation` VALUES (15505,609,0,7,0,5,0,0,0,0);
+INSERT INTO `creature_onkill_reputation` VALUES (15514,609,0,7,0,5,0,0,0,0);
+INSERT INTO `creature_onkill_reputation` VALUES (15538,609,0,7,0,5,0,0,0,0);
+INSERT INTO `creature_onkill_reputation` VALUES (15555,609,0,7,0,5,0,0,0,0);
+INSERT INTO `creature_onkill_reputation` VALUES (15934,609,0,7,0,5,0,0,0,0);
+INSERT INTO `creature_onkill_reputation` VALUES (15741,910,609,3,0,2000,7,0,25,0);
+
+-- Fixed Required Races for quest 6283 Bloodfury Bloodline
+UPDATE `quest_template` SET `RequiredRaces` = 178 WHERE `entry` = 6283;
+
+-- Fixed Required Races for quest 2936 The Spider God
+UPDATE `quest_template` SET `RequiredRaces` = 178 WHERE `entry` = 2936;
+
+-- Fixed Required Races for quest 8913 An earnest proposition
+UPDATE `quest_template` SET `RequiredRaces` = 178 WHERE `entry` = 8913;
+
+-- Fixed Required Races for quest 8927 Just Compensation
+UPDATE `quest_template` SET `RequiredRaces` = 178 WHERE `entry` = 8927;
+
 -- Tendriss Warpwood spawns Equine Spirit on Death, from old DB
 DELETE FROM `creature_ai_scripts` WHERE id = 1148906;
 INSERT INTO `creature_ai_scripts` VALUES (1148906,11489,6,0,100,0,0,0,0,0,32,14566,0,16,0,0,0,0,0,0,0,0,'Tendris Warpwood - Summon Ancient Equine Spirit on Death');
