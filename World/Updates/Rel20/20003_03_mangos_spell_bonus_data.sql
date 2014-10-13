9f3d8068ba815c5216792b76f6ff533d180af64b
Antz <billy1arm@users.noreply.github.com>
2014-10-14 00:07:25 +0100

    [World] Tidied up World Rel20 Updates

--- World/Updates/Rel20/20003_03_mangos_spell_bonus_data.sql
+++ World/Updates/Rel20/20003_03_mangos_spell_bonus_data.sql
@@ -1,7 +1,7 @@
-ALTER TABLE db_version CHANGE COLUMN required_20003_02_mangos_spell_bonus_data required_20003_03_mangos_spell_bonus_data bit;
+ALTER TABLE db_version CHANGE COLUMN required_20003_01_mangos_spell_bonus_data required_20003_02_mangos_spell_bonus_data bit;
 
--- Seal of Command fix
--- 20% SpellDamageBonusDone
--- 29% SpellDamageBonusTaken
-UPDATE `spell_bonus_data` SET `direct_bonus`='0', `direct_bonus_done`='0.2', `direct_bonus_taken`='0.29' WHERE `entry`='20424';
+-- Seal of Righteousness must get 12,5% spell bonus for 2-handed weapon and 10% spell bonus for 1-handed weapon.
+-- Trigerred spell must have 0% coeff.
+UPDATE `spell_bonus_data` SET `entry`='20154', `direct_bonus`='0', `one_hand_direct_bonus`='0.125', `two_hand_direct_bonus`='0.1' ,`ap_bonus`='0' WHERE `entry`='25742';
+
 
