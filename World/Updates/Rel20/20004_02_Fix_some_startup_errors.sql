81a31fcf4338b75ac9dfb2936a63b78840fe94c8
Antz <billy1arm@users.noreply.github.com>
2014-11-19 07:57:56 +0000

    Fixed some startup errors

---
+++ World/Updates/Rel20/20004_02_Fix_some_startup_errors.sql
@@ -0,0 +1,4 @@
+DELETE FROM creature_template_classlevelstats WHERE class=4;
+
+UPDATE creature_template SET AIName = 'EventAI' WHERE Entry = 3902;
+UPDATE creature_template SET AIName = 'EventAI' WHERE Entry = 16385;
