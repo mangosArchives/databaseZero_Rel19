a0d21ceaf5238ae00ae1799475f98c20c4acd8da
Antz <billy1arm@users.noreply.github.com>
2014-11-20 20:50:23 +0000

    Update 20004_05_Tauren_Starting_Zone_Braves_Should_Not_Speak_Fix.sql

--- World/Updates/Rel20/20004_05_Tauren_Starting_Zone_Braves_Should_Not_Speak_Fix.sql
+++ World/Updates/Rel20/20004_05_Tauren_Starting_Zone_Braves_Should_Not_Speak_Fix.sql
@@ -6,6 +6,9 @@
     Details on the NpcFlags field: https://github.com/cmangos/issues/wiki/creature_template
  */
 
++-- Add the Revision update into the revision column
++INSERT IGNORE INTO db_version SET `Version` = 'MaNGOSZero Database 2.0.11 Rev 05';
+
 -- Brave Darksky
 UPDATE creature_template SET `NpcFlags`='0' WHERE `Entry`='3220';
 -- Brave Dawneagle
