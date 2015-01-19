5bb6de58dabbe5ccabb48edd947558d040881b7f
Charles A Edwards <charles.a.edwards@btinternet.com>
2015-01-19 14:36:24 +0000

    Guardian of Blizzard fix 2
    Faction changed to Demon (73)

--- World/Updates/Rel20/20006_15_Guardian_of_Blizzard_non_combat_fix.sql
+++ World/Updates/Rel20/20006_15_Guardian_of_Blizzard_non_combat_fix.sql
@@ -3,4 +3,4 @@ INSERT IGNORE INTO db_version SET `Version` = 'MaNGOSZero Database 2.0.11 Rev 20
 
 -- Guardian of Blizzard non-combat fix (requires ScriptDev script fix)
 -- -- script fix: https://github.com/mangoszero/server/pull/285
-UPDATE creature_template SET AIName = 'EventAI' WHERE Entry=5764;
+UPDATE creature_template SET FactionAlliance = 73, FactionHorde = 73, AIName = 'EventAI' WHERE Entry = 5764;
