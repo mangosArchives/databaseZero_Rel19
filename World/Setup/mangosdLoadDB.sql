c020c0b7bac56550f04ac916a8a3c2148dd171b7
Antz <billy1arm@users.noreply.github.com>
2015-01-23 10:22:37 +0000

    [DBDOCS] add base tables to initial DB

--- World/Setup/mangosdLoadDB.sql
+++ World/Setup/mangosdLoadDB.sql
@@ -1179,16 +1179,71 @@ CREATE TABLE `db_script_string` (
 /*Table structure for table `dbdocsfields` */
 
 DROP TABLE IF EXISTS `dbdocsfields`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `dbdocsfields` (
+  `fieldId` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique Id for this field',
+  `languageId` int(11) NOT NULL DEFAULT '0' COMMENT 'dbdocsLanguageId to link to. (Normallu 0 = English)',
   `tableName` varchar(80) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of the table to link to',
   `fieldName` varchar(80) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of the field to link the note to',
+  `fieldComment` varchar(80) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Main field Note',
   `fieldNotes` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'Additional Field Notes',
-  PRIMARY KEY (`tableName`,`fieldName`)
+  PRIMARY KEY (`fieldId`,`languageId`),
+  KEY `fieldId` (`fieldId`)
+) ENGINE=InnoDB AUTO_INCREMENT=1764 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `dbdocsfields_localised`
+--
+DROP TABLE IF EXISTS `dbdocsfields_localised`;
+
+CREATE TABLE `dbdocsfields_localised` (
+  `fieldId` int(11) NOT NULL COMMENT 'dbdocsfields.fieldId to link to.',
+  `languageId` int(11) NOT NULL COMMENT 'dbdocsLanguageId to link to. (Normallu 0 = English)',
+  `fieldNotes` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'Additional Field Notes',
+  `fieldComment` varchar(80) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Main field Note',
+  PRIMARY KEY (`fieldId`,`languageId`),
+  KEY `fieldId` (`fieldId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-/*Table structure for table `dbdocsprogressquests` */
+/*Data for the table `dbdocsfields_localised` */
+LOCK TABLES `dbdocsfields_localised` WRITE;
+/*!40000 ALTER TABLE `dbdocsfields_localised` DISABLE KEYS */;
 
+/*!40000 ALTER TABLE `dbdocsfields_localised` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dbdocslanguage`
+--
+DROP TABLE IF EXISTS `dbdocslanguage`;
+
+CREATE TABLE `dbdocslanguage` (
+  `LanguageId` int(11) NOT NULL COMMENT 'LanguageId for this Language',
+  `LanguageName` varchar(30) NOT NULL COMMENT 'The Language Name',
+  PRIMARY KEY (`LanguageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `dbdocslanguage` */
+LOCK TABLES `dbdocslanguage` WRITE;
+/*!40000 ALTER TABLE `dbdocslanguage` DISABLE KEYS */;
+insert  into `dbdocslanguage`(`LanguageId`,`LanguageName`) values (0,'English');
+insert  into `dbdocslanguage`(`LanguageId`,`LanguageName`) values (1,'Korean');
+insert  into `dbdocslanguage`(`LanguageId`,`LanguageName`) values (2,'French');
+insert  into `dbdocslanguage`(`LanguageId`,`LanguageName`) values (3,'German');
+insert  into `dbdocslanguage`(`LanguageId`,`LanguageName`) values (4,'Chinese');
+insert  into `dbdocslanguage`(`LanguageId`,`LanguageName`) values (5,'Taiwanese');
+insert  into `dbdocslanguage`(`LanguageId`,`LanguageName`) values (6,'Spanish (Spain)');
+insert  into `dbdocslanguage`(`LanguageId`,`LanguageName`) values (7,'Spanish (Latin America)');
+insert  into `dbdocslanguage`(`LanguageId`,`LanguageName`) values (8,'Russian');
+/*!40000 ALTER TABLE `dbdocslanguage` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dbdocsprogressquests`
+--
 DROP TABLE IF EXISTS `dbdocsprogressquests`;
 
 CREATE TABLE `dbdocsprogressquests` (
@@ -1198,15 +1253,75 @@ CREATE TABLE `dbdocsprogressquests` (
   PRIMARY KEY (`QuestID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Table structure for table `dbdocstable` */
 
+--
+-- Table structure for table `dbdocssubtables`
+--
+DROP TABLE IF EXISTS `dbdocssubtables`;
+
+CREATE TABLE `dbdocssubtables` (
+  `subTableId` int(11) NOT NULL COMMENT 'Unique Lookup Id',
+  `languageId` int(11) NOT NULL DEFAULT '0' COMMENT 'dbdocsLanguageId to link to. (Normally 0 = English)',
+  `subTableName` varchar(80) DEFAULT NULL COMMENT 'Description of Content',
+  `subTableContent` text NOT NULL COMMENT 'The Sub Table Content',
+  `subTableTemplate` text NOT NULL COMMENT 'The Sub Table Template',
+  PRIMARY KEY (`subTableId`,`languageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Table structure for table `dbdocssubtables_localised`
+--
+DROP TABLE IF EXISTS `dbdocssubtables_localised`;
+
+CREATE TABLE `dbdocssubtables_localised` (
+  `subTableId` int(11) NOT NULL COMMENT 'dbdocsSubtableId to link to',
+  `languageId` int(11) NOT NULL DEFAULT '2' COMMENT 'dbdocsLanguageId to link to.',
+  `subTableContent` text NOT NULL COMMENT 'The Sub Table Content',
+  `subTableTemplate` text NOT NULL COMMENT 'The Sub Table Template',
+  PRIMARY KEY (`subTableId`,`languageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `dbdocssubtables_localised` */
+LOCK TABLES `dbdocssubtables_localised` WRITE;
+/*!40000 ALTER TABLE `dbdocssubtables_localised` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dbdocssubtables_localised` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dbdocstable`
+--
 DROP TABLE IF EXISTS `dbdocstable`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `dbdocstable` (
+  `tableId` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique ID of this entry',
+  `languageId` int(11) NOT NULL DEFAULT '0' COMMENT 'The Language Id for the Notes (Normally 0 for English)',
   `tableName` varchar(80) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of the table to add additional notes to',
   `tableNotes` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'The additional note to be added to the table',
-  PRIMARY KEY (`tableName`)
+  PRIMARY KEY (`tableId`,`languageId`,`tableName`),
+  KEY `tableId` (`tableId`)
+) ENGINE=InnoDB AUTO_INCREMENT=186 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `dbdocstable_localised`
+--
+DROP TABLE IF EXISTS `dbdocstable_localised`;
+
+CREATE TABLE `dbdocstable_localised` (
+  `tableId` int(11) NOT NULL COMMENT 'The dbdocsTableId to link to',
+  `languageId` int(11) NOT NULL DEFAULT '2' COMMENT 'The dbdocsLanguageId to link to',
+  `tableNotes` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'The additional note to be added to the table',
+  PRIMARY KEY (`tableId`,`languageId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `dbdocstable_localised` */
+LOCK TABLES `dbdocstable_localised` WRITE;
+/*!40000 ALTER TABLE `dbdocstable_localised` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dbdocstable_localised` ENABLE KEYS */;
+UNLOCK TABLES;
 --
 -- Table structure for table `dbscripts_on_creature_death`
 --
