d6279d1dedcc344e150df4af15839c2299d92ca2
Olion <oleg70ua@yahoo.com>
2015-01-30 19:28:33 +0200

    Reverting wrong file

--- World/Updates/Rel20/20006_26_Quest_2203_requirements.sql
+++
@@ -1,5 +0,0 @@
--- Add the Revision update into the revision column
-INSERT IGNORE INTO `db_version` SET `Version` = 'MaNGOSZero Database 2.0.11 Rev 20006_26';
-
--- Quest 2203 (Badlands Reagent Run II)
-UPDATE quest_template SET RequiredSkillValue = 1 WHERE entry = 2203;
